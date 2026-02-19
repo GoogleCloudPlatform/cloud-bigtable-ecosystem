a9ac11faf62e005c20f405799adbb9e5eb7f0a38
Adam Czajkowski <prawilny@unoperate.com>
2026-02-19 13:01:35 +0100

    Format sink code

--- kafka-connect-bigtable-sink/integration-tests/src/test/java/com/google/cloud/kafka/connect/bigtable/integration/BaseIT.java
+++ kafka-connect-bigtable-sink/integration-tests/src/test/java/com/google/cloud/kafka/connect/bigtable/integration/BaseIT.java
@@ -26,11 +26,9 @@ import com.google.cloud.bigtable.data.v2.BigtableDataClient;
 import com.google.cloud.kafka.connect.bigtable.config.BigtableSinkConfig;
 import com.google.cloud.kafka.connect.bigtable.util.TestId;
 import com.google.cloud.kafka.connect.bigtable.wrappers.BigtableTableAdminClientInterface;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.connect.runtime.ConnectorConfig;
 import org.apache.kafka.connect.storage.StringConverter;
@@ -64,7 +62,8 @@ public abstract class BaseIT {
         String.valueOf(maxKafkaMessageSizeBytes));
 
     result.put(GCP_PROJECT_ID_CONFIG, Objects.requireNonNull(System.getenv(GCP_PROJECT_ID)));
-    result.put(BIGTABLE_INSTANCE_ID_CONFIG, Objects.requireNonNull(System.getenv(BIGTABLE_INSTANCE_ID)));
+    result.put(
+        BIGTABLE_INSTANCE_ID_CONFIG, Objects.requireNonNull(System.getenv(BIGTABLE_INSTANCE_ID)));
     result.put(
         BigtableSinkConfig.GCP_CREDENTIALS_PATH_CONFIG,
         Objects.requireNonNull(System.getenv(CREDENTIALS_PATH_ENV_VAR)));
